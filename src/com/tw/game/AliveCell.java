e2e462d4017a6fc243ad0751b1441245604963ac
priyadarshini <priyadr@thoughtworks.com>
2015-09-19 12:13:02 +0530

    refactoring alive cell

--- src/com/tw/game/AliveCell.java
+++ src/com/tw/game/AliveCell.java
@@ -10,15 +10,20 @@ public class AliveCell extends Cell {
     }
 
     public Cell nextState() {
+        List<Cell> nextNeighbours = nextStateNeighbours();
+        int aliveNeighbours = getNumberOfAliveNeighbours();
+        if ((aliveNeighbours < 2) || (aliveNeighbours > 3))
+            return new DeadCell(nextNeighbours);
+        return this;
+    }
+
+    public List<Cell> nextStateNeighbours(){
         List<Cell> nextNeighbours = null;
         if (neighbours != null) {
             nextNeighbours = new ArrayList<Cell>();
             for (Cell neighbour : neighbours)
                 nextNeighbours.add(neighbour.nextState());
         }
-        int aliveNeighbours = getNumberOfAliveNeighbours();
-        if ((aliveNeighbours < 2) || (aliveNeighbours > 3))
-            return new DeadCell(nextNeighbours);
-        return this;
+        return nextNeighbours;
     }
 }
