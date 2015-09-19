6e61220691ec9ee108a679d17030fb2b85341f3e
priyadarshini <priyadr@thoughtworks.com>
2015-09-19 10:26:57 +0530

    cell remains in the same state

--- src/com/tw/game/Cell.java
+++ src/com/tw/game/Cell.java
@@ -12,6 +12,6 @@ public class Cell {
     }
 
     public String nextState() {
-        return "dead";
+        return currentState;
     }
 }
--- test/com/tw/game/CellTest.java
+++ test/com/tw/game/CellTest.java
@@ -11,4 +11,10 @@ public class CellTest {
         Cell cell = new Cell("dead",0,8);
         assertEquals(cell.nextState(), "dead");
     }
+
+    @Test
+    public void shouldReturnNextStateAsCurrentState(){
+        Cell cell = new Cell("alive",2,6);
+        assertEquals(cell.nextState(), "alive");
+    }
 }
